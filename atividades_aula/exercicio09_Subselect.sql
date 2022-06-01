977f5d3f8e9ff127d2dcfbade78d6d19dde3c62c
Johnny William <50759662+Jownao@users.noreply.github.com>
2022-06-01 10:42:26 -0300

    Update exercicio09_Subselect.sql

--- atividades_aula/exercicio09_Subselect.sql
+++ atividades_aula/exercicio09_Subselect.sql
@@ -16,35 +16,39 @@ where (mgp) >   (select avg(mgp)
 order by 2,1 asc
 
 --Cláusura where - listar o nome do aluno e o nome do curso para os alunos com maior e menor quantidade de créditos cursados por curso
-select  a.nom_alu, c.nom_curso
-from alunos a join cursos c on a.cod_curso = c.cod_curso
-where (c.cod_curso,a.tot_cred) in (select c.cod_curso,max(a.tot_cred),min(a.tot_cred)
-                                from alunos a join cursos c on a.cod_curso = c.cod_curso and a.tot_cred > 0
-                                group by c.cod_curso)
-
-select c.nom_curso, a.nom_alu, a.tot_cred
+select  a.nom_alu, c.nom_curso, a.tot_cred
 from alunos a join cursos c using(cod_curso)
-where (cod_curso, a.tot_cred) in (select cod_curso, max(tot_cred)
-                                  from alunos
-                                  group by cod_curso)
-or (cod_curso, a.tot_cred) in (select cod_curso, min(tot_cred)
-                                  from alunos
-                                  group by cod_curso)
-
-
-
-
+where (cod_curso,a.tot_cred) in (select cod_curso,min(tot_cred)
+                                from alunos 
+                                where tot_cred > 0
+                                group by cod_curso)
+or (cod_curso,a.tot_cred) in (select cod_curso,max(tot_cred)
+                                from alunos 
+                                group by cod_curso)
 
 --Cláusura from - listar o nome do aluno e o nome da disciplina dos alunos que já perderam a mesma disciplina mais de uma vez
-
+select nom_disc, nom_alu 
+from (select *
+      from alunos a join historicos h using(mat_alu) 
+      where h.situacao in ('RM','RF')) d 
+      join disciplinas d using (cod_disc)
+group by nom_disc, nom_alu
+having count(nom_disc) > 1
 
 --Cláusura from - além de seguir os critérios do item 4, liste somente os casos em que a média do aluno foi inferior a 4,0 na disciplina e o número de faltas superior a 18
-
+select nom_disc, nom_alu
+from (select * 
+      from alunos a join historicos h using(mat_alu)
+      where h.situacao in ('RM','RF')) d
+      join disciplinas d using (cod_disc)
+where mgp < 4.0 or faltas > 18
+group by nom_disc, nom_alu
+having count(nom_disc) > 1
 
 --Cláusura from - listar os 5 alunos com maior média na disciplina 282
-
 select nom_alu 
-  from (select mgp
-         from alunos
-         order by mgp desc) 
-where rownum <= 5 and cod_disc = 282
+  from (select nom_alu
+         from alunos join historicos using(mat_alu)
+         where cod_disc = 282
+         order by media desc) 
+where rownum <= 5
